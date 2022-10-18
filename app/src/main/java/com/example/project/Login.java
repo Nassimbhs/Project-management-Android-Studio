bba79fdd48b0dd1a31e9e53d70590ed58046a869
Nassim Ben Hassine <79005429+Nassimbhs@users.noreply.github.com>
2022-10-18 22:50:51 +0100

    Home page

--- app/src/main/java/com/example/project/Login.java
+++ app/src/main/java/com/example/project/Login.java
@@ -1,11 +1,13 @@
 package com.example.project;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -40,16 +42,17 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                SharedPreferences sharedPreferences = getSharedPreferences("sp",MODE_PRIVATE);
+                SharedPreferences sharedPreferences = getSharedPreferences("sp", MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
 
-                editor.putString("email",email.getText().toString());
-                editor.putString("password",password.getText().toString());
-                editor.commit();
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+                editor.putString("email", email.getText().toString());
+                editor.putString("password", password.getText().toString());
+                editor.apply();
 
-            }
+                    Intent intent = new Intent(Login.this, Acceuil.class);
+                    startActivity(intent);
+                }
+
         });
 
     }
